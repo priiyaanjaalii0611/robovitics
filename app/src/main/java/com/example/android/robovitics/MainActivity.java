d8fae2ca3f0b1abf1463203e14e5be1acdd0da41
priiyaanjaalii0611 <priyanjali.gupta2019@vitstudent.ac.in>
2021-01-29 22:50:50 +0530

    first app

---
+++ app/src/main/java/com/example/android/robovitics/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.android.robovitics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void welcome(View view){
+
+        Toast.makeText(this,"WELCOME TO ROBOVITICS",Toast.LENGTH_SHORT).show();
+    }
+}
